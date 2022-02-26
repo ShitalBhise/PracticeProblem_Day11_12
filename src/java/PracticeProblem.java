3b16a99d32d7c24db73ddc03825f95828351a9fe
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-26 20:48:03 +0530

    Welcome

---
+++ src/java/PracticeProblem.java
@@ -0,0 +1,10 @@
+package java;
+
+public class PracticeProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to practice problem day 11 12");
+	}
+
+}
